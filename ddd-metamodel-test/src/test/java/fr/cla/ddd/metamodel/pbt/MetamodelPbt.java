ce561384fe7358d7a2831ae37d18c82b35f304e4
Laurent Claisse <you@example.com>
2019-04-12 10:56:18 +0200

    magic numbers

--- ddd-metamodel-test/src/test/java/fr/cla/ddd/metamodel/pbt/MetamodelPbt.java
+++ ddd-metamodel-test/src/test/java/fr/cla/ddd/metamodel/pbt/MetamodelPbt.java
@@ -4,6 +4,7 @@ public final class MetamodelPbt {
 
     public static final int SANITY_CHECK_TRIALS = 10_000;
 
-    public static final int PROPERTY_TRIALS = 100_000;
+    //If we need N trials to be sure to have non trivial input, we need more to test the funtionnality
+    public static final int PROPERTY_TRIALS = 10 * SANITY_CHECK_TRIALS;
 
 }
