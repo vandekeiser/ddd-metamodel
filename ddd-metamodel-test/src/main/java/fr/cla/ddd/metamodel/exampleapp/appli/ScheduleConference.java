f63fb45d42758e4aa148f0234c0d4a519ed9338a
Laurent Claisse <you@example.com>
2019-05-01 15:24:14 +0200

    comments

--- ddd-metamodel-test/src/main/java/fr/cla/ddd/metamodel/exampleapp/appli/ScheduleConference.java
+++ ddd-metamodel-test/src/main/java/fr/cla/ddd/metamodel/exampleapp/appli/ScheduleConference.java
@@ -25,12 +25,11 @@ public class ScheduleConference {
         this.conferences = requireNonNull(conferences);
     }
 
-    /**
-     * The rest api might have validated the schema, but it also must take into account
-     * (eg. translate them to 400)
-     */
     public void scheduleConference(ScheduleConferenceCommand cmd) {
         log.info("Received command {}", cmd);
+        //The rest api might have validated the schema,
+        // but we also must take into account validations that can only be done in the backend
+        //(eg. translate them to 400).
         SrtConference conf = ApplicativeValidations.validateApplicatively(cmd, ScheduleConferenceCommand::createConference);
         log.info("Created {} to add it", conf);
         conferences.add(conf);
