6a4c27df0696cf790571543e1849b96b83c71acc
Laurent Claisse <you@example.com>
2019-05-02 14:24:39 +0200

    generate spring cloud contract test

---
+++ ddd-metamodel-test/src/test/java/fr/cla/ddd/metamodel/exampleapp/expo/AbstractContractVerifierTest.java
@@ -0,0 +1,46 @@
+package fr.cla.ddd.metamodel.exampleapp.expo;
+
+import fr.cla.ddd.metamodel.appli.validation.ApplicativeValidations;
+import fr.cla.ddd.metamodel.exampleapp.appli.ScheduleConference;
+import fr.cla.ddd.metamodel.exampleapp.appli.ScheduleConferenceCommand;
+import fr.cla.ddd.metamodel.exampleapp.expo.conference.ConferenceResource;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+
+import static org.mockito.Mockito.any;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public abstract class AbstractContractVerifierTest {
+
+    @Autowired
+    ConferenceResource bookController;
+
+    @MockBean
+    private ScheduleConference scheduleConference;
+
+    @BeforeAll
+    public void setup() {
+        Mockito.doAnswer(validateApplicatively()).when(scheduleConference).scheduleConference(any(ScheduleConferenceCommand.class));
+        StandaloneMockMvcBuilder standaloneMockMvcBuilder = MockMvcBuilders.standaloneSetup(bookController);
+        RestAssuredMockMvc.standaloneSetup(standaloneMockMvcBuilder);
+    }
+
+    private Answer<Void> validateApplicatively() {
+        return invocationOnMock -> {
+            ScheduleConferenceCommand cmd = invocationOnMock.getArgument(0);
+            ApplicativeValidations.validateApplicatively(cmd, ScheduleConferenceCommand::createConference);
+            return null;
+        };
+    }
+
+}
---
+++ ddd-metamodel-test/src/test/resources/contracts/contract-for-when-total-cost-does-not-exceed-budget.groovy
@@ -0,0 +1,43 @@
+package contracts
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    name("contract-for-when-total-cost-does-not-exceed-budget")
+    description """
+Represents the success scenario for scheduling a conference
+ 
+```
+given:
+ the conference's budget is 1000 euros 
+and:
+ the conference's has a talk that cost<s 400 euros 
+and:
+ the conference's has a talk that costs 600 euros 
+when:
+ the user tries to schedule the conference
+then:
+ it is accepted
+```
+ 
+"""
+    request {
+        method 'POST'
+        urlPath('/example-app/conferences/')
+        body(
+            budget: 1000,
+            costs: [400, 600]
+        )
+        headers {
+            contentType("application/json;charset=UTF-8")
+            accept("application/json;charset=UTF-8")
+        }
+    }
+
+    response {
+        status 201
+        headers {
+            contentType("application/json;charset=UTF-8")
+        }
+    }
+}
